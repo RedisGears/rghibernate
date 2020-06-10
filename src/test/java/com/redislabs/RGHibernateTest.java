38416c99c659969e1bd029eb7b5d11d8972f15aa
Guy Korland <gkorland@gmail.com>
2020-06-10 20:57:19 +0300

    fix tests

--- src/test/java/com/redislabs/RGHibernateTest.java
+++ src/test/java/com/redislabs/RGHibernateTest.java
@@ -25,7 +25,7 @@ public class RGHibernateTest {
 
       Transaction transaction = session.beginTransaction();
       Map item1 = new HashMap();
-      item1.put( "id", 33);
+      item1.put( "id", "33");
       item1.put( "firstName", "Ron" );
       item1.put( "lastName", "Don" );
       item1.put( "email", "ron.don@tau.ac.il" );
@@ -34,7 +34,7 @@ public class RGHibernateTest {
       
       Transaction transaction2 = session.beginTransaction();
       Map item2 = new HashMap();
-      item2.put( "id", 12);
+      item2.put( "id", "12");
       item2.put( "firstName", "Danni" );
       item2.put( "lastName", "Din" );
       item2.put( "email", "danni.din@tau.ac.il" );
@@ -48,7 +48,7 @@ public class RGHibernateTest {
 
       Transaction transaction3 = session.beginTransaction();
       Map item3 = new HashMap();
-      item3.put( "id", 51);
+      item3.put( "id", "51");
       item3.put( "firstName", "John");
       item3.put( "lastName", "Scott");
       item3.put( "email", "john.scott@tau.ac.il");
@@ -57,7 +57,7 @@ public class RGHibernateTest {
       
       Transaction transaction4 = session.beginTransaction();
       Map item4 = new HashMap();
-      item4.put( "id", 12);
+      item4.put( "id", "12");
       item4.put( "firstName", "Danni");
       item4.put( "lastName", "Chin");
       item4.put( "email", "danni.chin@tau.ac.il");
