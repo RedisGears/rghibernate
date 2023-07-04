df3340375d02725f5035e2f7498bac7f323a8c65
Chayim <chayim@users.noreply.github.com>
2023-07-04 16:23:26 +0300

    [Snyk] Upgrade org.hibernate:hibernate-core from 5.6.7.Final to 6.1.0.Final (#83)
    * feat: upgrade org.hibernate:hibernate-core from 5.6.7.Final to 6.1.0.Final
    
    Snyk has created this PR to upgrade org.hibernate:hibernate-core from 5.6.7.Final to 6.1.0.Final.
    
    See this package in Maven Repository:
    https://mvnrepository.com/artifact/org.hibernate/hibernate-core/
    
    See this project in Snyk:
    https://app.snyk.io/org/redislabs/project/64ab0f78-129a-4790-89b5-3cc2720af6f7?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * Fix build error
    
    * Update JVM version
    
    * Fix compilation error
    
    * Install sqlalchemy
    
    * Update deps to work properly
    
    * Remove sql alchamy installation
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Meir Shpilraien (Spielrein) <meir@redis.com>

--- src/main/java/com/redislabs/PropertyData.java
+++ src/main/java/com/redislabs/PropertyData.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.sql.Date;
 
 import org.hibernate.type.AbstractStandardBasicType;
-import org.hibernate.type.TimestampType;
+import org.hibernate.type.DbTimestampType;
 import org.hibernate.type.Type;
 
 public class PropertyData implements Serializable{
@@ -39,7 +39,7 @@ public class PropertyData implements Serializable{
       // best effort, keep it as string.
       return val;
     }
-    if(this.type instanceof TimestampType) {
+    if(this.type instanceof DbTimestampType) {
       // try first to parse as timestamp
       try {
         long timestamp = Long.parseLong(val);
@@ -52,7 +52,7 @@ public class PropertyData implements Serializable{
   }
   
   public String convertToStr(Object val) {
-    if(this.type instanceof TimestampType && val instanceof java.sql.Timestamp) {
+    if(this.type instanceof DbTimestampType && val instanceof java.sql.Timestamp) {
       // try first to parse as timestamp
       try {
         return Long.toString(((java.sql.Timestamp)val).getTime());
